da00a9bd37fc7000e4c7f4a324e66ae0e7a0a10a
SerjiVutinss <SerjiVutinss@gmail.com>
2019-04-27 15:22:28 +0100

    Commetning and tidy up

--- Assets/_scripts/ShimmerJointOrientation.cs
+++ Assets/_scripts/ShimmerJointOrientation.cs
@@ -1,66 +1,64 @@
 ﻿using UnityEngine;
-
-//using LockingPolicy = Thalmic.Myo.LockingPolicy;
-//using UnlockType = Thalmic.Myo.UnlockType;
 using ShimmerRT.models;
 using UnityEngine.UI;
 
-// Orient the object to match that of the Myo armband.
-// Compensate for initial yaw (orientation about the gravity vector) and roll (orientation about
-// the wearer's arm) by allowing the user to set a reference orientation.
-// Making the fingers spread pose or pressing the 'r' key resets the reference orientation.
+// Orient the object to match that of the Shimmer IMU device.
+// 'The Transform' referred to in the comments of this class refers to the transform of 
+// the object to which this script is attached (i.e. the 'Joint' object in the Unity scene)
 public class ShimmerJointOrientation : MonoBehaviour
 {
-    // Myo game object to connect with.
-    // This object must have a ThalmicMyo script attached.
-    //public GameObject shimmerDevice = null;
-
-    // A rotation that compensates for the Myo armband's orientation parallel to the ground, i.e. yaw.
-    // Once set, the direction the Myo armband is facing becomes "forward" within the program.
-    // Set by making the fingers spread pose or pressing "r".
-    private Quaternion _antiYaw = Quaternion.identity;
-
+    #region User adjustable variables
+    // value used to determine whether an impact has occurred
+    // TODO: should be possible for the user to set this in the UI
     public float impactThreshold = 1.0f;
-
-    // A reference angle representing how the armband is rotated about the wearer's arm, i.e. roll.
-    // Set by making the fingers spread pose or pressing "r".
-    private float _referenceRoll = 0.0f;
-
-    private ShimmerDevice shimmerFeed;
-
-    public GameObject SceneManager;
-
-    private Vector3 accelerometer;
-    private Vector3 gyroscope;
-
+    // allow adjustment of transform to desired rotation
+    // TODO: should be possible for the user to set this in the UI, not just in inspector
     public Vector3 offset = new Vector3(0, 0, 0);
+    #endregion
 
-    private Shimmer3DModel lastShimmerModel = null;
-
+    #region References to Unity GameObjects and Scripts
+    // reference to a shimmer device - live data can be retrieved from this device and applied to the transform
+    private ShimmerDevice shimmerDevice;
+    // reference to the SceneManager which controls most of the UI
+    public GameObject SceneManager;
+    // UI element used to inform user when an impact has occurred
     public Text txtImpact;
+    #endregion
 
-    // variables used for playback
+    #region Private script variables
+    // variable used in impact checking to compare a model to the previous one applied to the transform
+    private Shimmer3DModel lastShimmerModel = null;
+    #endregion
+
+    #region Playback
     private bool isLive = true;
-    private double firstFrameTime;
-    private double timeSinceLastFrame;
+    private double firstModelTime; // first model time - timestamp of first model in playback list
+    private double timeSinceLastModelApplied; // time since last model applied
+    #endregion
+
+    #region reference variables for roll and anti-yaw
+    private float _referenceRoll = 0.0f;
+    private Quaternion _antiYaw = Quaternion.identity;
+    #endregion
 
     void Start()
     {
-        // get the script from the ShimmerDevice object
-        shimmerFeed = SceneManager.GetComponent<ShimmerDevice>();
-        //ResetTransform();
+        // get the script from the ShimmerDevice object available through the reference to the SceneManager
+        shimmerDevice = SceneManager.GetComponent<ShimmerDevice>();
     }
 
+    // main logic used to decide what to do with the transform, i.e. live streaming or playback
+    // TODO: tidy up the logic in this method if needed
     private void Update()
     {
         // handle live streaming from shimmer
-        if (!shimmerFeed.IsPlayback)
+        if (!shimmerDevice.IsPlayback)
         {
             isLive = true;
             // if data is available, use it
-            if (shimmerFeed.Queue.Count > 0)
+            if (shimmerDevice.Queue.Count > 0)
             {
-                var s = shimmerFeed.Queue.Dequeue();
+                var s = shimmerDevice.Queue.Dequeue();
                 // see if there was an 'impact' between this data and the last received data
                 if (lastShimmerModel != null)
                 {
@@ -74,114 +72,63 @@ public class ShimmerJointOrientation : MonoBehaviour
                 UpdateTransform(s);
                 lastShimmerModel = s;
             }
-        }
-        else
+        } // end handle live streaming
+        else // handle playback
         {
-            // handle playback from memory if data is present
+            // handle playback from memory if data is present and playback mode is enabled
             Shimmer3DModel s;
-            if (shimmerFeed.RecordList.Count > 0 && shimmerFeed.RecordList != null && shimmerFeed.IsPlayback == true)
+            if (shimmerDevice.RecordList.Count > 0 && shimmerDevice.RecordList != null && shimmerDevice.IsPlayback == true)
             {
-
-                // TODO: update this code to account for time between each shimmer model
-
                 // get the first model from the list
-                s = shimmerFeed.RecordList[0];
-
-                // see if this is the first frame of data
+                s = shimmerDevice.RecordList[0];
+                // check whether this is the first model in the playback list
+                // - if it is the first model, isLive will still be set to true
                 if (isLive)
                 {
                     // first frame, set these variables
                     isLive = false;
-                    // this will be set as the zero frame time
-                    firstFrameTime = s.Timestamp_CAL;
-                    timeSinceLastFrame = 0.0;
+                    // firstModelTime is set as the timestamp of the first model in the list, all other models
+                    // will be applied to the transform relative to this first model
+                    firstModelTime = s.Timestamp_CAL;
 
                     // update the model's transform accordingly
                     UpdateTransform(s);
+                    // reset the time since last model applied, since one was just applied
+                    timeSinceLastModelApplied = 0.0;
                     // and remove the 'used' model from the list
-                    shimmerFeed.RecordList.Remove(s);
+                    shimmerDevice.RecordList.Remove(s);
                 }
 
                 double timeToWait = 0.0;
                 if (!isLive)
                 {
-                    timeToWait = (s.Timestamp_CAL - firstFrameTime) / 1000;
+                    timeToWait = (s.Timestamp_CAL - firstModelTime) / 1000;
                 }
 
-                if (timeSinceLastFrame >= timeToWait)
+                // check if the next model should be applied to the transform
+                // i.e. wait the correct amount of time to display the next frame
+                if (timeSinceLastModelApplied >= timeToWait)
                 {
                     // update the model's transform accordingly
                     UpdateTransform(s);
                     // and remove the 'used' model from the list
-                    shimmerFeed.RecordList.Remove(s);
+                    shimmerDevice.RecordList.Remove(s);
                 }
                 else
                 {
-                    timeSinceLastFrame += Time.deltaTime;
+                    // still waiting, increment the time since last frame with time between this 
+                    timeSinceLastModelApplied += Time.deltaTime;
                 }
-
-                // wait the correct amount of time to display the next frame
-
-
-                //shimmerFeed.IsPlayback = false;
+            }
+            else
+            {
+                // playback is either disabled or has ended, simply turn playback off to save on processing
+                shimmerDevice.IsPlayback = false;
             }
         }
     }
 
-    #region
-
-    private bool CheckImpact(Shimmer3DModel s)
-    {
-
-        float dX = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_X_CAL - s.Low_Noise_Accelerometer_X_CAL));
-        float dY = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_Y_CAL - s.Low_Noise_Accelerometer_Y_CAL));
-        float dZ = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_Z_CAL - s.Low_Noise_Accelerometer_Z_CAL));
-
-        //Debug.Log(dX);
-        //Debug.Log(dY);
-        //Debug.Log(dZ);
-
-        if (dX > impactThreshold)
-        {
-            return true;
-        }
-        if (dY > impactThreshold)
-        {
-            return true;
-        }
-        if (dZ > impactThreshold)
-        {
-            return true;
-        }
-        return false;
-    }
-    #endregion
-
-    //void ResetTransform()
-    //{
-    //    // Current zero roll vector and roll value.
-    //    Vector3 zeroRoll = computeZeroRollVector(shimmerDevice.transform.forward);
-    //    float roll = rollFromZero(zeroRoll, shimmerDevice.transform.forward, shimmerDevice.transform.up);
-
-    //    // The relative roll is simply how much the current roll has changed relative to the reference roll.
-    //    // adjustAngle simply keeps the resultant value within -180 to 180 degrees.
-    //    float relativeRoll = normalizeAngle(roll - _referenceRoll);
-
-    //    // antiRoll represents a rotation about the myo Armband's forward axis adjusting for reference roll.
-    //    Quaternion antiRoll = Quaternion.AngleAxis(relativeRoll, shimmerDevice.transform.forward);
-
-    //    // Here the anti-roll and yaw rotations are applied to the myo Armband's forward direction to yield
-    //    // the orientation of the joint.
-    //    transform.rotation = _antiYaw * antiRoll * Quaternion.LookRotation(shimmerDevice.transform.forward);
-
-    //    // Mirror the rotation around the XZ plane in Unity's coordinate system (XY plane in Myo's coordinate
-    //    // system). This makes the rotation reflect the arm's orientation, rather than that of the Myo armband.
-    //    transform.rotation = new Quaternion(transform.localRotation.x,
-    //                                        -transform.localRotation.y,
-    //                                        transform.localRotation.z,
-    //                                        -transform.localRotation.w);
-    //}
-
+    // apply a Shimmer model to the transform
     private void UpdateTransform(Shimmer3DModel s)
     {
 
@@ -211,28 +158,61 @@ public class ShimmerJointOrientation : MonoBehaviour
             -(float)s.Gyroscope_X_CAL);
     }
 
+    #region Impact 
+    private bool CheckImpact(Shimmer3DModel s)
+    {
+
+        float dX = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_X_CAL - s.Low_Noise_Accelerometer_X_CAL));
+        float dY = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_Y_CAL - s.Low_Noise_Accelerometer_Y_CAL));
+        float dZ = Mathf.Abs((float)(lastShimmerModel.Low_Noise_Accelerometer_Z_CAL - s.Low_Noise_Accelerometer_Z_CAL));
+
+        //Debug.Log(dX);
+        //Debug.Log(dY);
+        //Debug.Log(dZ);
+
+        if (dX > impactThreshold)
+        {
+            return true;
+        }
+        if (dY > impactThreshold)
+        {
+            return true;
+        }
+        if (dZ > impactThreshold)
+        {
+            return true;
+        }
+        return false;
+    }
+    #endregion
+
+    #region Legacy accelerometer and gyro variables
+    private Vector3 accelerometer;
+    private Vector3 gyroscope;
+    #endregion
+    #region Legacy code from Myo code samples
     // Compute the angle of rotation clockwise about the forward axis relative to the provided zero roll direction.
     // As the armband is rotated about the forward axis this value will change, regardless of which way the
     // forward vector of the Myo is pointing. The returned value will be between -180 and 180 degrees.
-    float rollFromZero(Vector3 zeroRoll, Vector3 forward, Vector3 up)
-    {
-        // The cosine of the angle between the up vector and the zero roll vector. Since both are
-        // orthogonal to the forward vector, this tells us how far the Myo has been turned around the
-        // forward axis relative to the zero roll vector, but we need to determine separately whether the
-        // Myo has been rolled clockwise or counterclockwise.
-        float cosine = Vector3.Dot(up, zeroRoll);
+    //float rollFromZero(Vector3 zeroRoll, Vector3 forward, Vector3 up)
+    //{
+    //    // The cosine of the angle between the up vector and the zero roll vector. Since both are
+    //    // orthogonal to the forward vector, this tells us how far the Myo has been turned around the
+    //    // forward axis relative to the zero roll vector, but we need to determine separately whether the
+    //    // Myo has been rolled clockwise or counterclockwise.
+    //    float cosine = Vector3.Dot(up, zeroRoll);
 
-        // To determine the sign of the roll, we take the cross product of the up vector and the zero
-        // roll vector. This cross product will either be the same or opposite direction as the forward
-        // vector depending on whether up is clockwise or counter-clockwise from zero roll.
-        // Thus the sign of the dot product of forward and it yields the sign of our roll value.
-        Vector3 cp = Vector3.Cross(up, zeroRoll);
-        float directionCosine = Vector3.Dot(forward, cp);
-        float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+    //    // To determine the sign of the roll, we take the cross product of the up vector and the zero
+    //    // roll vector. This cross product will either be the same or opposite direction as the forward
+    //    // vector depending on whether up is clockwise or counter-clockwise from zero roll.
+    //    // Thus the sign of the dot product of forward and it yields the sign of our roll value.
+    //    Vector3 cp = Vector3.Cross(up, zeroRoll);
+    //    float directionCosine = Vector3.Dot(forward, cp);
+    //    float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
 
-        // Return the angle of roll (in degrees) from the cosine and the sign.
-        return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
-    }
+    //    // Return the angle of roll (in degrees) from the cosine and the sign.
+    //    return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
+    //}
 
     // Compute a vector that points perpendicular to the forward direction,
     // minimizing angular distance from world up (positive Y axis).
@@ -247,18 +227,18 @@ public class ShimmerJointOrientation : MonoBehaviour
     //}
 
     // Adjust the provided angle to be within a -180 to 180.
-    float normalizeAngle(float angle)
-    {
-        if (angle > 180.0f)
-        {
-            return angle - 360.0f;
-        }
-        if (angle < -180.0f)
-        {
-            return angle + 360.0f;
-        }
-        return angle;
-    }
+    //float normalizeAngle(float angle)
+    //{
+    //    if (angle > 180.0f)
+    //    {
+    //        return angle - 360.0f;
+    //    }
+    //    if (angle < -180.0f)
+    //    {
+    //        return angle + 360.0f;
+    //    }
+    //    return angle;
+    //}
 
     // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
     // recognized.
@@ -273,4 +253,31 @@ public class ShimmerJointOrientation : MonoBehaviour
 
     //    myo.NotifyUserAction();
     //}
+
+    //void ResetTransform()
+    //{
+    //    // Current zero roll vector and roll value.
+    //    Vector3 zeroRoll = computeZeroRollVector(shimmerDevice.transform.forward);
+    //    float roll = rollFromZero(zeroRoll, shimmerDevice.transform.forward, shimmerDevice.transform.up);
+
+    //    // The relative roll is simply how much the current roll has changed relative to the reference roll.
+    //    // adjustAngle simply keeps the resultant value within -180 to 180 degrees.
+    //    float relativeRoll = normalizeAngle(roll - _referenceRoll);
+
+    //    // antiRoll represents a rotation about the myo Armband's forward axis adjusting for reference roll.
+    //    Quaternion antiRoll = Quaternion.AngleAxis(relativeRoll, shimmerDevice.transform.forward);
+
+    //    // Here the anti-roll and yaw rotations are applied to the myo Armband's forward direction to yield
+    //    // the orientation of the joint.
+    //    transform.rotation = _antiYaw * antiRoll * Quaternion.LookRotation(shimmerDevice.transform.forward);
+
+    //    // Mirror the rotation around the XZ plane in Unity's coordinate system (XY plane in Myo's coordinate
+    //    // system). This makes the rotation reflect the arm's orientation, rather than that of the Myo armband.
+    //    transform.rotation = new Quaternion(transform.localRotation.x,
+    //                                        -transform.localRotation.y,
+    //                                        transform.localRotation.z,
+    //                                        -transform.localRotation.w);
+    //}
+
+    #endregion
 }
